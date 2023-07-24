43dafb4bba2b9dd8df71df9ec96501412a6fb9b6
Abulfadl Ah <abulfadl.ahmadi@gmail.com>
2023-07-24 21:11:18 +0330

    fix navbar active

---
+++ js.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+    // ...
+    // Place code here.
+    // ...
+    var d = document.getElementById("projects");
+    d.className += "  active";
+    // document.body.classList.add('red-back');
+
+
+});
